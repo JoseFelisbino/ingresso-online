c97d7bff7555fa80c59a50a2e8a18d91f4efc478
JoseFelisbino <lu31660@gmail.com>
2026-04-28 23:54:21 -0300

    feat: compra para tipo pista

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    alert(tipo.value);
-    alert(qtd.value);
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if (qtd > qtdPista.textContent) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!')
+    }
 }
